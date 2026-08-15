f03d96d8676a82bc836ad7908091dd481f430ae3
Kush Chaudhari <kushchaudhari48@gmail.com>
2026-08-15 14:31:50 +0530

    Add automatic superuser creation script

--- core/settings.py
+++ core/settings.py
@@ -74,6 +74,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,6 +86,9 @@ DATABASES = {
         }
     }
 }
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
